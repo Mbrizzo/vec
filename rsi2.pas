6e63387678143047bcc364dc0dbbecfd2a5b4914
Marcel <mrizzo25@gmail.com>
2023-05-14 01:07:57 -0300

    2p - paint

--- rsi2.pas
+++ rsi2.pas
@@ -6,14 +6,14 @@ mp_rsi : float;
 mp_prev_rsi : float;
 
 begin
-r_rsi := RSI(3, 0);
+r_rsi := RSI(2, 0);
 r_prev_rsi := r_rsi[1];
 mp_rsi := WAverage(Close, 2);
 mp_prev_rsi := mp_rsi[1];
 
 if ((r_prev_rsi < mp_prev_rsi) and ( r_rsi > mp_rsi))  then
-        plotText("Buy",clGreen, 0)
+    PaintBar(clBlue)
 else if (r_prev_rsi > mp_prev_rsi) and (r_rsi < mp_rsi) then
-    PlotText("Sell", clRed, -1);
+    PaintBar(clYellow);
 
 end
